3b8277a1b3166321ea2a03eee58350c2bbb7a582
proyectoTecnologico2023 <146397740+proyectoTecnologico2023@users.noreply.github.com>
2023-10-15 17:37:18 -0300

    Delete pagProy directory

--- pagProy/paginasHTML/footer.php
+++
@@ -1,9 +0,0 @@
-    <footer>
-        <div class="footer-content">
-            <p>&copy; 2023 POLITICANDO. Todos los derechos reservados.</p>
-            <p>Contacto: info@politicando.com</p>
-        </div>    
-    </footer>
-</body>
-</html>
-<!-- FOOTER PARA PAGINAS. Evitar repetir codigo -->
